41fd794f6dac2af4110a66b25a05f6c9af5dca18
LEONARDO <leonardoaxo@gmail.com>
2021-03-21 01:31:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import { RepositoryList } from './components/RepositoryList';
+import './Style/global.scss';
+export function App(){
+    return <RepositoryList />
+}
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,27 @@
+export function RepositoryList() {
+    return (
+      <section className = "repository-list">
+          <h1>Lista de repositorios </h1>
+          <ul>
+              <li>
+                  <strong>Unform</strong>
+                  <a href ="">
+                      Acessar repositorio
+                  </a>
+              </li>
+              <li>
+                  <strong>Unform</strong>
+                  <a href ="">
+                      Acessar repositorio
+                  </a>
+              </li>
+              <li>
+                  <strong>Unform</strong>
+                  <a href ="">
+                      Acessar repositorio
+                  </a>
+              </li>
+          </ul>
+      </section>
+  );
+}
