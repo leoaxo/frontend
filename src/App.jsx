6d02df0a39feafc4033b6a59afbb2cc0ec70e3fe
LEONARDO <leonardoaxo@gmail.com>
2021-03-22 22:07:41 -0300

    modificação final

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-import { RepositoryList } from './components/RepositoryList';
-import './Style/global.scss';
-export function App(){
-    return <RepositoryList />
-}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import { RepositoryList } from './components/RepositoryList';
+//import './Style/global.scss';
+
+export function App(){
+    return (
+        <>
+
+    <RepositoryList />
+   
+    </>
+    );}
